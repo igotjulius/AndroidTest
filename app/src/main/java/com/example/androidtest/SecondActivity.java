20e2e96d23d3993e33c5d282a660411cf75a3239
Julius Wilmar <wilmarjulius@gmail.com>
2023-02-18 18:21:11 +0800

    February 18, 2023 - Linear, Relative, Constraint Layout

--- app/src/main/java/com/example/androidtest/SecondActivity.java
+++ app/src/main/java/com/example/androidtest/SecondActivity.java
@@ -1,8 +1,14 @@
 package com.example.androidtest;
 
+import static com.example.androidtest.MainActivity.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +16,16 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button button = (Button) findViewById(R.id.button4);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button Clicked");
+                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
