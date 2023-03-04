d2c606355fe077af87fa4dbedbde6fd17cc86f4a
Julius Wilmar Igot <wilmarjulius@gmail.com>
2023-03-04 17:12:55 +0800

    Inputs Activity

---
+++ app/src/main/java/com/example/androidtest/ListActivity.java
@@ -0,0 +1,19 @@
+package com.example.androidtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
---
+++ app/src/main/java/com/example/androidtest/my_list.java
@@ -0,0 +1,14 @@
+package com.example.androidtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class my_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_list);
+    }
+}
